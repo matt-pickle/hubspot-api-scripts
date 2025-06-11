cd0e17d99e984d1f58ec085709d3fa665a3b5102
Matt Pickle <mattpickle@mattpickle.net>
2025-06-11 15:18:47 -0500

    Fix variable snake case

--- functions/batch_delete_records.py
+++ functions/batch_delete_records.py
@@ -1,8 +1,8 @@
 import requests
 import time
 
-def batch_delete_records(recordType: str, ids: list[str], PRIVATE_APP_KEY: str) -> None:
-    url = f"https://api.hubapi.com/crm/v3/objects/{recordType}/batch/archive"
+def batch_delete_records(record_type: str, ids: list[str], PRIVATE_APP_KEY: str) -> None:
+    url = f"https://api.hubapi.com/crm/v3/objects/{record_type}/batch/archive"
     headers = { "Authorization": f"Bearer {PRIVATE_APP_KEY}", "Content-Type": "application/json"}
 
     for i in range(0, len(ids), 100):
@@ -16,8 +16,8 @@ def batch_delete_records(recordType: str, ids: list[str], PRIVATE_APP_KEY: str) 
         try:
             response = requests.post(url, headers=headers, json=data)
             response.raise_for_status()
-            print(f"Deleted {recordType}: {len(batch)}")
+            print(f"Deleted {record_type}: {len(batch)}")
         except requests.exceptions.RequestException as e:
-            print(f"Error deleting {recordType}: {e}")
+            print(f"Error deleting {record_type}: {e}")
 
         time.sleep(0.25)
